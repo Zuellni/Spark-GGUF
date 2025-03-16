58696f162fab8869602edf968fabbafe0394aee5
Zuellni <123005779+Zuellni@users.noreply.github.com>
2025-03-16 20:27:11 +0100

    Squeeze audio tensor, use omegaconf for loading, suppress some warnings, fix requirements

--- models.py
+++ models.py
@@ -1,19 +1,19 @@
+import contextlib
 import re
 import sys
+import warnings
 from pathlib import Path
 from typing import Literal
-from warnings import simplefilter
 
-simplefilter("ignore")
+warnings.simplefilter("ignore")
 
+import huggingface_hub as hf
 import numpy as np
 import torch
 import torchaudio
-import yaml
 from faster_whisper import WhisperModel
-from huggingface_hub import hf_hub_download, snapshot_download
 from llama_cpp import Llama
-from torchaudio import functional as F
+from omegaconf import OmegaConf
 from transformers import Wav2Vec2FeatureExtractor, Wav2Vec2Model
 
 sys.path.append(str(Path(__file__).parent / "spark_tts"))
@@ -27,29 +27,29 @@ class Codec:
         wav2vec2: str = "annuvin/wav2vec2",
         device: str = "cuda",
         dtype: torch.dtype = torch.float16,
-        duration: int = 6,
+        ref_len: int = 6,
     ) -> None:
         if not Path(model).is_dir():
-            model = snapshot_download(model)
+            model = hf.snapshot_download(model)
 
         if not Path(wav2vec2).is_dir():
-            wav2vec2 = snapshot_download(wav2vec2)
+            wav2vec2 = hf.snapshot_download(wav2vec2)
 
         model = Path(model)
         wav2vec2 = Path(wav2vec2)
-        config = yaml.safe_load((model / "config.yaml").read_bytes())
+        config = OmegaConf.load(model / "config.yaml")
         hop_length = config["audio_tokenizer"]["mel_params"]["hop_length"]
         self.sample_rate = config["audio_tokenizer"]["mel_params"]["sample_rate"]
 
-        self.model = BiCodec.load_from_checkpoint(model).to(device, dtype)
-        self.processor = Wav2Vec2FeatureExtractor.from_pretrained(wav2vec2)
-        self.extractor = Wav2Vec2Model.from_pretrained(
-            pretrained_model_name_or_path=wav2vec2,
-            torch_dtype=dtype,
-        ).to(device)
-        self.extractor.config.output_hidden_states = True
+        with contextlib.redirect_stdout(None):
+            self.model = BiCodec.load_from_checkpoint(model).to(device, dtype)
 
-        self.ref_len = int(self.sample_rate * duration) // hop_length * hop_length
+        self.processor = Wav2Vec2FeatureExtractor.from_pretrained(wav2vec2)
+        self.extractor = Wav2Vec2Model.from_pretrained(wav2vec2, torch_dtype=dtype)
+        self.extractor.config.output_hidden_states = True
+        self.extractor.to(device)
+
+        self.ref_len = int(self.sample_rate * ref_len) // hop_length * hop_length
         self.pattern = re.compile(r"<\|bicodec_semantic_(\d+)\|>")
         self.device = device
         self.dtype = dtype
@@ -58,26 +58,26 @@ class Codec:
         wav, sample_rate = torchaudio.load(path)
         wav = wav.to(self.device, self.dtype)
 
-        if wav.shape[0] > 1:
-            wav = torch.mean(wav, dim=0, keepdim=True)
+        if len(wav) > 1:
+            wav = torch.mean(wav, dim=0)
 
         if sample_rate != self.sample_rate:
-            wav = F.resample(wav, sample_rate, self.sample_rate)
+            wav = torchaudio.functional.resample(wav, sample_rate, self.sample_rate)
 
-        return wav
+        return wav.squeeze()
 
     def _process(self, wav: torch.Tensor) -> torch.Tensor:
-        if wav.shape[1] < self.ref_len:
-            wav = torch.tile(wav, (1, self.ref_len // wav.shape[1] + 1))
+        if len(wav) < self.ref_len:
+            wav = torch.tile(wav, (0, self.ref_len // len(wav) + 1))
 
-        return wav[:, : self.ref_len]
+        return wav[: self.ref_len]
 
     def encode(self, path: str) -> tuple[torch.Tensor, str]:
         wav = self._load(path)
         ref = self._process(wav)
 
         inputs = self.processor(
-            raw_speech=wav.squeeze(),
+            raw_speech=wav,
             output_hidden_states=True,
             padding=True,
             return_tensors="pt",
@@ -86,6 +86,8 @@ class Codec:
 
         feat = self.extractor(inputs).hidden_states
         feat = (feat[11] + feat[14] + feat[16]) / 3.0
+        wav = wav.unsqueeze(0)
+        ref = ref.unsqueeze(0)
 
         _, tensor = self.model.tokenize({"wav": wav, "ref_wav": ref, "feat": feat})
         token_str = "".join([f"<|bicodec_global_{t}|>" for t in tensor.squeeze()])
@@ -107,15 +109,16 @@ class Spark:
         flash_attn: bool = True,
     ) -> None:
         if not Path(model).is_file():
-            model = hf_hub_download(model, f"model.{dtype}.gguf")
+            model = hf.hf_hub_download(model, f"model.{dtype}.gguf")
 
-        self.model = Llama(
-            model_path=model,
-            n_gpu_layers=-1,
-            n_ctx=context,
-            flash_attn=flash_attn,
-            verbose=False,
-        )
+        with contextlib.redirect_stderr(None), contextlib.redirect_stdout(None):
+            self.model = Llama(
+                model_path=model,
+                n_gpu_layers=-1,
+                n_ctx=context,
+                flash_attn=flash_attn,
+                verbose=False,
+            )
 
     def encode(
         self, text: str, add_bos: bool = False, special: bool = False
